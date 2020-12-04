0f573f7ffeeae076c46b117f0d55540eb60f28b7
jfspps <jfsapps@gmail.com>
2020-12-04 14:06:27 +0000

    first commit of basicAssignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WarningAlert } from './warning-alert/warningalert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlert,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warning-alert/warningalert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-warning_alert',
+    templateUrl: './warningalert.component.html',
+    styles: ['h1 { color: darkred }']
+})
+export class WarningAlert{
+
+}
