d78ecfb1b85a86aee6a851e97423312946062db8
jfspps <jfsapps@gmail.com>
2020-12-04 15:34:11 +0000

    finished styling

--- src/app/warning-alert/warningalert.component.ts
+++ src/app/warning-alert/warningalert.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-warning_alert',
     templateUrl: './warningalert.component.html',
-    styles: ['h1 { color: darkred }']
+    styles: ['h1 { color: darkred; background-color: pink; }']
 })
 export class WarningAlert{
 
